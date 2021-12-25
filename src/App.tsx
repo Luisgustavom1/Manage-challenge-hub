792949e53e2c2f5b4df968b300b4be4959170ea1
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-12-25 17:32:36 -0300

    Config prettier eslint e editorconfig

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Home from './Views/Home';
+
+function App() {
+  return (
+    <Home />
+  )
+}
+
+export default App
---
+++ src/Views/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../../Components/Header";
+
+function Home() {
+  return <Header />;
+}
+
+export default Home;
